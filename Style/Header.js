d7aca6980371d280518bac478255e16110ef3ce4
Garrett Parker <g.parker@monroeisd.us>
2021-02-22 14:16:30 -0500

    Initial

---
+++ Style/Header.js
@@ -0,0 +1,26 @@
+document.write(`
+	<div class="navigation">
+		<div class="brand">
+			<div class="inline-block">
+				<a href="https://garrettspage.com/index.html" >GARRETT PARKER</a>
+			</div>
+		</div>
+		<nav class="nav-block">
+			<div class="nav-link" style="width: 115px">
+				<div class="inline-block">
+					<a href="https://garrettspage.com/Projects/Project-Hub.html" >PROJECTS</a>
+				</div>
+			</div>
+			<div class="nav-link" style="width: 90px">
+				<div class="inline-block">
+					<a href="https://garrettspage.com/Memes/Meme-Hub.html" >MEMES</a>
+				</div>
+			</div>
+			<div class="nav-link-big" style="width: 150px">
+				<div class="inline-block">
+					<a href="https://garrettspage.com/Documents/Document-Hub.html" >DOCUMENTS</a>
+				</div>
+			</div>
+		</nav>
+	</div>
+`);
