4c5378546bc2cd7ec8b2ff0730cd9d1c944992ee
Garrett Parker <g.parker@monroeisd.us>
2021-02-22 20:25:37 -0500

    DNPGames

--- Style/Footer.js
+++
@@ -1,7 +0,0 @@
-document.write(`
-	<div class="footer-copyright">
-		<div class="inline-block">
-			<a onClick="javascript:window.open('https://private.garrettspage.com', '_self')">© 2021 DNP Games</a>
-		</div>
-	</div>
-`);
